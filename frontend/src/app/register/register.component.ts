d9dd664a7be293b5dab0d207d73ebfcb7f005d3b
Emre Ceran <emre.ceran@students.unibe.ch>
2019-11-30 00:36:59 +0100

    Added toasts instead of Alerts

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -3,6 +3,7 @@ import {LoginService} from '../_services/login.service';
 import {User} from '../_models/user';
 import {RegisterService} from '../_services/register.service';
 import {Router} from '@angular/router';
+import {ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -13,18 +14,24 @@ export class RegisterComponent implements OnInit {
 
   user = new User(-1, '', '', '');
 
-  constructor(public router: Router) { }
+  constructor(
+    public router: Router,
+    public toastController: ToastController
+  ) { }
 
   ngOnInit() {}
 
-  // Change this for registration
+
   onRegisterDisplay() {
     RegisterService.Register(this.user.email, this.user.password, this.user.name).subscribe((instance: any) => {
         this.user = new User(instance.id, instance.name, instance.email, instance.password);
         alert('got in');
+        this.openToast('Registration successful. Redirecting to login page.');
+        this.router.navigate(['/login']);
       },
       (error) => {
         alert('not found' + error);
+        this.openToast('Registration failed.');
         console.log(error);
       });
     console.log(this.user);
@@ -32,6 +39,17 @@ export class RegisterComponent implements OnInit {
 
   onLoginDisplay(){
     this.router.navigate(['/login']);
+  }
+
+  async openToast(output) {
+    const toast = await this.toastController.create({
+      message: output,
+      duration: 4000,
+      position: 'top',
+      showCloseButton: true,
+      color: 'light',
+    });
+    toast.present();
   }
 }
 
