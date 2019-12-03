6f5068887b73b5a3e3210ca752a91277018e1911
Emre Ceran <emre.ceran@students.unibe.ch>
2019-12-03 21:23:55 +0100

    fixed bug of path

---
+++ backend/app/models/service.model.ts
@@ -0,0 +1,34 @@
+import {Table, Column, Model, HasMany, BelongsTo, ForeignKey} from 'sequelize-typescript';
+
+
+@Table
+export class Service extends Model<Service> {
+
+
+  @Column
+  name!: string;
+
+  @Column
+  email !: string;
+
+  @Column
+  password!: string;
+
+
+  toSimplification(): any {
+    return {
+      'id': this.id,
+      'name': this.name,
+      'email': this.email,
+      'password': this.password
+    };
+  }
+
+  fromSimplification(simplification: any): void {
+    this.name = simplification['name'];
+    this.email = simplification['email'];
+    this.password = simplification['password'];
+    this.id = simplification['id'];
+  }
+
+}
--- frontend/src/app/dashboard/dashboard.module.ts
+++ frontend/src/app/dashboard/dashboard.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     component: DashboardPage,
     children: [
       {
-        path: '',
+        path: 'dashboard',
         redirectTo: 'profile'
       },
       {
